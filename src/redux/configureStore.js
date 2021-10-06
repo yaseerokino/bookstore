9b81c20b638ffa0f9aadc950ab389772d90f6eeb
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-10-06 14:20:20 +0100

    Add middleware functions

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,6 +3,7 @@ import {
 } from 'redux';
 import { composeWithDevTools as devTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import bookReducer from './books/books';
 
 const reducer = combineReducers({
@@ -11,7 +12,7 @@ const reducer = combineReducers({
 
 const store = createStore(
   reducer,
-  compose(applyMiddleware(logger), devTools()),
+  compose(applyMiddleware(thunk), applyMiddleware(logger), devTools()),
 );
 
 export default store;
